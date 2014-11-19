aa32d5b0de48b440b5654b5cb8fbf239a55667ec
Bryan <brndmfrd@gmail.com>
2014-11-18 22:47:55 -0500

    Angular front end reads data from the DB via the API and parses the json from it.

--- site/models/post.js
+++ site/models/post.js
@@ -1,3 +1,5 @@
+// Called by Node (/server.js) 
+// This is our api model that connects us to mongoose (see /db.js)
 var db = require('../db')
 var Post = db.model('Post', {
   body: { type: String, required: true }
--- site/server.js
+++ site/server.js
@@ -11,17 +11,25 @@ server.listen(port, function(){
   console.log('Server listen at port %d', port);
 });
 
-// Routing
 
+
+// Routing
 app.use(bodyParser.json())
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/public'));
 //app.use(require('./controllers'));
 
 
+
+// Wrap everything in the scope of socket.io
 io.on('connection', function (socket) {
   socket.emit('login', 'bob');
 
+
+
+// POST requests API connected to our DB
+// var Post connects us to our model post.js that 
+// connects us to the mongo db via mongoose (/db.js)
 var Post = require('./models/post')
 app.post('/api/posts', function (req, res, next) {
   var post = new Post({
@@ -35,6 +43,10 @@ app.post('/api/posts', function (req, res, next) {
   socket.broadcast.emit('user joined', 'robert');  
 });
 
+
+// GET requests API connected to our DB 
+// var Post connects us to our model post.js that 
+// connects us to the mongo db via mongoose (/db.js)
 app.get('/api/posts', function(req, res, next){
   Post.find(function(err, posts) {
     if(err){return next(err)}
@@ -42,6 +54,8 @@ app.get('/api/posts', function(req, res, next){
   });
 });
 
+
+// This is where we serve index.html through node
 app.get('/', function(req, res){
   res.sendfile('/public/index.html')
 });
